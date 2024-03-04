45d708188cbbc0588d2cf9ac92391468d442c611
Lukas-kozel <l.kozel@outlook.cz>
2024-03-04 17:14:51 +0100

    base version

--- GasStationSimulation.java
+++ GasStationSimulation.java
@@ -3,24 +3,19 @@ import javaSimulation.Process;
 
 public class GasStationSimulation extends Process {
     int noOfPumps;
-    int noOfCashiers;
     double simPeriod = 300;
     Head availablePumps = new Head();
-    Head availableCashiers = new Head(); 
-    Head waitingCars = new Head();//fronta čekajících aut v čerpací stanici
-    Head waitingForPayment = new Head();//fronta čekající na placení
+    Head waitingCars = new Head();
     Random random = new Random(5);
     double totalServiceTime;
     int noOfServedCars, maxQueueLength;
     long startTime = System.currentTimeMillis();
 
-    GasStationSimulation(int n, int m) { noOfPumps = n; noOfCashiers=m;}
+    GasStationSimulation(int n) { noOfPumps = n; }
 
     public void actions() {
         for (int i = 0; i < noOfPumps; i++)
             new FuelPump().into(availablePumps);
-        for (int i = 0; i < noOfCashiers; i++)
-            new Cashier().into(availableCashiers);
         activate(new CarGenerator());
         hold(simPeriod + 1000000);
         report();
@@ -28,7 +23,6 @@ public class GasStationSimulation extends Process {
 
     void report() {
         System.out.println(noOfPumps + " fuel pump simulation");
-        System.out.println(noOfCashiers + " cashiers simulation");
         System.out.println("Number of cars served = " + noOfServedCars);
         java.text.NumberFormat fmt = java.text.NumberFormat.getNumberInstance();
         fmt.setMaximumFractionDigits(2);
@@ -53,49 +47,33 @@ public class GasStationSimulation extends Process {
         public void actions() {
             double arrivalTime = time();
             into(waitingCars);
-            while (true) {
-                if (!availablePumps.empty()) {
-                    activate((FuelPump) availablePumps.first());
-                }
-                passivate(); // Auto čeká na dokončení tankování
-                
-                // Po dokončení tankování auto přejde k placení
-                into(waitingForPayment);
-                if (!availableCashiers.empty()) {
-                    activate((Cashier) availableCashiers.first());
-                }
-                passivate(); // Auto čeká na dokončení placení
-                break; // Po dokončení placení je proces auta ukončen
-            }
-            totalServiceTime += time() - arrivalTime;
+            int queueLength = waitingCars.cardinal();
+            if (maxQueueLength < queueLength)
+                maxQueueLength = queueLength;
+            if (!availablePumps.empty())
+                activate((FuelPump) availablePumps.first());
+            passivate();
             noOfServedCars++;
-            out(); // Odstranění auta z procesu
+            totalServiceTime += time() - arrivalTime;
         }
-        //metoda pro placení
-        public void startPayment() {
-        into(waitingForPayment);
-        if (!availableCashiers.empty()) {
-            activate((Cashier) availableCashiers.first());
-        }
-        passivate(); // Auto čeká, dokud nebude placení dokončeno
-    }
     }
 
     class FuelPump extends Process {
         public void actions() {
             while (true) {
-                if (!waitingCars.empty()) {
+                out();
+                while (!waitingCars.empty()) {
                     Car car = (Car) waitingCars.first();
-                    car.out(); // Odstranění auta z fronty čekajících na tankování
-                    double fuelNeeded = 100 - car.getCarFuelLevel();
+                    car.out();
+                    // Přepočet doby tankování v závislosti na chybějícím palivu
+                    double fuelNeeded = 100 - car.getCarFuelLevel(); // maximální kapacita nádrže je 100%
                     double refuelingTime = calculateRefuelingTime(fuelNeeded);
                     hold(refuelingTime); // Simulace doby tankování
-                    activate(car); // Přesun auta k placení
+                    activate(car);
                 }
-                passivate(); // Pumpa čeká na další auto
+                wait(availablePumps);
             }
         }
-        
         // Metoda pro výpočet doby tankování - předpoklad, že existuje přímá závislost mezi chybějícím palivem a dobou tankování
         // předpokladem je, že natankování 100% nádrže trvá 10 minut
         private double calculateRefuelingTime(double fuelNeeded) {
@@ -103,40 +81,17 @@ public class GasStationSimulation extends Process {
         }
     }
 
-    class Cashier extends Process {
-        public void actions() {
-            while (true) {
-                if (!waitingForPayment.empty()) {
-                    Car car = (Car) waitingForPayment.first();
-                    car.out(); // Odstranění auta z fronty čekajících na placení
-                    double paymentTime = calculatePaymentTime();
-                    hold(paymentTime); // Simulace doby placení
-                    activate(car);
-                }
-                passivate();
-                //wait(availableCashiers);
-            }
-        }
-
-        private double calculatePaymentTime() {
-            double mean = 2;
-            double std = 1;
-            return Math.abs(random.nextGaussian() * std + mean);
-        }
-    }
-
     class CarGenerator extends Process {
         public void actions() {
             while (time() <= simPeriod) {
-                //activate(new Car());
-                new Car().into(waitingCars);
+                activate(new Car());
                 hold(random.negexp(1 / 10.0));
             }
         }
     }
     public static void main(String args[]) {
-        activate(new GasStationSimulation(1,1));
-        activate(new GasStationSimulation(2,2));
-        activate(new GasStationSimulation(4,4));
+        activate(new GasStationSimulation(1));
+        activate(new GasStationSimulation(2));
+        activate(new GasStationSimulation(4));
     } 
 }
